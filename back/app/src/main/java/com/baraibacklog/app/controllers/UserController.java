d94dca155b1a9bd1007f3ef31224fc54f2df6935
Alex-Guima <alexandreagtaveira@gmail.com>
2023-05-30 17:13:45 -0300

    Create UserController

---
+++ back/app/src/main/java/com/baraibacklog/app/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.baraibacklog.app.controllers;
+
+import com.baraibacklog.app.models.User;
+import com.baraibacklog.app.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = this.userService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    @Validated(User.CreateUser.class)
+    public ResponseEntity<Void> create(@Valid @RequestBody User obj) {
+        this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated(User.UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id) {
+        obj.setId(id);
+        this.userService.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.userService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
