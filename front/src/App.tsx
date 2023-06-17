44b843fe75ab3f149201cb59e12e1929848ab269
AllRez44 <allanresendegomes@outlook.com>
2023-06-17 01:23:58 -0300

    Revert "fix file sync of local volume with barai_frontend docker container's volume"
    This reverts commit 7473487e1b2f8051b23c4be423217420b63644d1.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Clica em mim, porr
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -7,8 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 8000, // This is the port which we will use in docker
-    watch: {
-      usePolling: true,
-    },
   }
 })
